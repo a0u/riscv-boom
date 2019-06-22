95e0cb5c8a0dbe4e20e7812f9313050dbb8cac90
Ben Korpan <bkorpan@berkeley.edu>
2019-06-21 22:50:21 -0700

    dec_rdy logic supports new and old fetch buffers.

--- src/main/scala/exu/core.scala
+++ src/main/scala/exu/core.scala
@@ -416,7 +416,8 @@ class BoomCore(implicit p: Parameters, edge: freechips.rocketchip.tilelink.TLEdg
   }
 
   // all decoders are empty and ready for new instructions
-  dec_rdy := dec_will_fire.last
+  if (useNewFetchBuffer) dec_rdy := dec_will_fire.last
+  else                   dec_rdy := !dec_last_inst_was_stalled
 
   when (dec_rdy || io.ifu.clear_fetchbuffer) {
     dec_finished_mask := 0.U
